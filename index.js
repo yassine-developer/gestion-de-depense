3afe8d19bb3ca926c71bac29ecf6eea24387a3cf
yassine-developer <mah.yassine1981@gmail.com>
2025-05-09 11:25:01 +0200

    deuxieme mise a jour

---
+++ index.js
@@ -0,0 +1,58 @@
+const depenses = [];
+const listes = document.querySelector(".liste-depenses ul");
+const totale = document.querySelector("#totale");
+
+document.querySelector("button").addEventListener('click', (e) => {
+    e.preventDefault();
+    const nom = document.getElementById("description").value;
+    const montant = parseInt(document.getElementById("montant").value, 10);
+    const categorie = document.getElementById("categorie").value;
+
+
+
+    depenses.push([nom, montant, categorie]);
+
+    document.getElementById("description").value = "";
+    document.getElementById("montant").value = "";
+    let somme = 0;
+    listes.innerHTML = "";
+    depenses.forEach((element, index) => {
+        const li = document.createElement("li");
+        li.innerHTML = `
+        ${element[0]} ${element[1]} ${element[2]}
+        <button class="supprimer" data-index="${index}">supprimer</supprimer>
+        `;
+        listes.appendChild(li);
+        console.log(li);
+        somme += element[1];
+    });
+
+
+    totale.innerText = `${somme}`;
+
+})
+
+listes.addEventListener('click', (e) => {
+
+    if (e.target.classList.contains('supprimer')) {
+        let listeNum = e.target.dataset.index;
+        depenses.splice(listeNum, 1);
+
+        let somme = 0;
+        listes.innerHTML = "";
+        depenses.forEach((element, index) => {
+            const li = document.createElement("li");
+            li.innerHTML = `
+        ${element[0]} ${element[1]} ${element[2]}
+        <button class="supprimer" data-index="${index}">supprimer</supprimer>
+        `;
+            listes.appendChild(li);
+            console.log(li);
+            somme += element[1];
+        })
+        totale.innerText = `${somme}`;
+    }
+
+
+
+})
